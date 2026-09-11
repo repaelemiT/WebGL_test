dd9d44b5c9f849ee72b7635294d9dd5951ae4ba3
repaelemiT <m202410662@xs.ustb.edu.cn>
2026-09-11 22:43:04 +0800

    v17

--- git_push.sh
+++ git_push.sh
@@ -1,4 +1,18 @@
+proj_pre="WebGL_"
 commit_name="$1"
+
+build="Build"
+SA="StreamingAssets"
+idx="index.html"
+
+rm -rf ./$build
+rm -rf ./$SA
+rm ./$idx
+
+cur_dir=$proj_pre$commit_name
+cp -r ../$cur_dir/$build ./$build
+cp -r ../$cur_dir/$SA ./$SA
+cp ../$cur_dir/$idx ./$idx
 
 git add .
 git commit -m $commit_name
